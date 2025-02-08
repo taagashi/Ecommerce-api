8c95719d64c33291113e149367dac012d6657527
Thauã <tagashi.dev@gmail.com>
2025-02-08 20:57:59 -0300

    First commit

---
+++ src/test/java/br/com/thaua/Ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thaua.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
